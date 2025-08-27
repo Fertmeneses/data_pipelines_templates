829ec446b6d30c6e8949cc6e07558ae4db87b4e6
Fertmeneses_HPvictus <fertmeneses@gmail.com>
2025-08-27 08:54:50 -0300

    Add function plot_target_dist

--- data_exploration.py
+++ data_exploration.py
@@ -77,3 +77,58 @@ def load_dataset(
     return df, ids if id_col is not None else df
 
 # ===================
+# Plot target distribution
+
+def plot_target_dist(
+    df, target_col, pie_threshold=10, reg_bins=50
+    ):
+    """
+    Plot the target distribution in a dataset.
+    --- Inputs ---
+    {df} [pandas.DataFrame]: Input dataframe.
+    {target_col} [str]: Name of the target column.
+    {pie_threshold} [int]: For a classification task, it defines the maximum number
+    of classes for which the output will be a pie chart, else it will be a bar plot.
+    {reg_bins} [int]: For regression, define the number of bins for the histogram.
+    
+    --- Output ---
+    - If the target column is numeric and has more than {pie_threshold} unique values,
+    then plot a histogram.
+    - If the target column is categorical (or numeric with few unique values):
+        - Pie chart if classes <= {pie_threshold}.
+        - Horizontal bar plot if classes > {pie_threshold}.
+    """
+    # Identify target column:
+    target = df[target_col]
+    nunique = target.nunique()
+    target_type = 'Numeric' if pd.api.types.is_numeric_dtype(target) else 'Object'
+    
+    # If target is numeric and have few unique values, plot histogram:
+    if pd.api.types.is_numeric_dtype(target) and nunique > pie_threshold:
+        plt.figure(figsize=(8, 5))
+        plt.hist(target, bins=reg_bins, edgecolor="navy", alpha=0.7)
+        plt.title(f"Histogram of {target_col} (Regression)")
+        plt.xlabel(target_col)
+        plt.ylabel("Frequency")
+        plt.show()
+    # Else, plot either a pie chart of horizontal bar plot:
+    else:
+        value_counts = target.value_counts().sort_values(ascending=False)
+        if nunique <= pie_threshold:
+            # Pie chart
+            plt.figure(figsize=(6, 6))
+            wedges, texts, autotexts = plt.pie(
+                value_counts,labels=None,autopct="%1.1f%%",startangle=90)
+            plt.title(f"Class distribution of {target_col} ({target_type})")
+            plt.legend(wedges, value_counts.index,
+                title="Classes",loc="center left",bbox_to_anchor=(1, 0, 0.5, 1))
+            plt.show()
+        else:
+            # Horizontal bar plot
+            plt.figure(figsize=(10, 6))
+            value_counts.plot(kind="barh", color="steelblue", edgecolor="black")
+            plt.title(f"Class distribution of {target_col} ({target_type})")
+            plt.xlabel("Frequency")
+            plt.ylabel("Classes")
+            plt.gca().invert_yaxis()
+            plt.show() 
